9b3672818ce8817596747999c153bb63aca82ff0
Rohith  B <rohith@Rohiths-MacBook-Air.local>
2024-05-31 16:34:37 +0530

    modified

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, redirect, render_template, url_for, send_file
+from flask import Flask, request, redirect, render_template, url_for, send_file, flash
 from flask_pymongo import PyMongo
-import csv
-import os
 from bson import ObjectId
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SelectField, validators
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Set a secret key for session security
 
 # MongoDB configuration
 app.config["MONGO_URI"] = "mongodb+srv://rohith3085:rohith_db_5803@cluster0.a9hpefq.mongodb.net/user_info?retryWrites=true&w=majority&tlsInsecure=true"
@@ -40,6 +41,14 @@ class User:
         self.question18 = question18
         self.question19 = question19
 
+class UserForm(FlaskForm):
+    name = StringField('Name', validators=[validators.InputRequired()])
+    age = IntegerField('Age', validators=[validators.InputRequired()])
+    gender = SelectField('Gender', choices=[('male', 'Male'), ('female', 'Female')], validators=[validators.InputRequired()])
+    number = StringField('Phone Number', validators=[validators.InputRequired(), validators.Length(min=10, max=10)])
+    email = StringField('Email', validators=[validators.InputRequired(), validators.Email()])
+    # Add validation for other questions if needed
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -58,7 +67,8 @@ def download():
 
 @app.route('/quiz')
 def quiz():
-    return render_template('quiz.html')
+    form = UserForm()
+    return render_template('quiz.html', form=form)
 
 @app.route('/loader')
 def loader():
@@ -70,38 +80,26 @@ def thankyou():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    try:
-        user = User(
-            name=request.form['name'],
-            age=request.form['age'],
-            gender=request.form['gender'],
-            number=request.form['number'],
-            email=request.form['email'],
-            question1=request.form['question1'],
-            question2=request.form['question2'],
-            question3=request.form['question3'],
-            question4=request.form['question4'],
-            question5=request.form['question5'],
-            question6=request.form['question6'],
-            question7=request.form['question7'],
-            question8=request.form['question8'],
-            question9=request.form['question9'],
-            question10=request.form['question10'],
-            question11=request.form['question11'],
-            question12=request.form['question12'],
-            question13=request.form['question13'],
-            question14=request.form['question14'],
-            question15=request.form['question15'],
-            question16=request.form['question16'],
-            question17=request.form['question17'],
-            question18=request.form['question18'],
-            question19=request.form['question19']
-        )
-        user_data = user.__dict__
-        mongo.db.users.insert_one(user_data)
-        return redirect('/loader')
-    except Exception as e:
-        return str(e), 500
+    form = UserForm(request.form)
+    if form.validate():
+        try:
+            user = User(
+                name=form.name.data,
+                age=form.age.data,
+                gender=form.gender.data,
+                number=form.number.data,
+                email=form.email.data,
+                # Add other questions from the form if needed
+            )
+            user_data = user.__dict__
+            mongo.db.users.insert_one(user_data)
+            return redirect('/loader')
+        except Exception as e:
+            flash('An error occurred while processing your request.')
+            return redirect(url_for('quiz'))
+    else:
+        flash('Please fill out all required fields correctly.')
+        return redirect(url_for('quiz'))
 
 @app.route('/export-csv', methods=['GET'])
 def export_csv():
