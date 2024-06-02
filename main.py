975bcd17217c32eb6b5b146cd7c7487311968b6d
Rohith  B <rohith@Rohiths-MacBook-Air.local>
2024-06-03 02:54:44 +0530

    username-and-pass-changed

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import os
 
 app = Flask(__name__)
 
-# MongoDB Atlas configuration
-mongo_uri = 'mongodb+srv://rohith3085:xz6tjuQKjA7WDpjj@cluster0.hfretfn.mongodb.net/know-your-sleep-genetics?retryWrites=true&w=majority&appName=Cluster0'
+# MongoDB Atlas configuration with the new URI and password
+mongo_uri = 'mongodb+srv://rohith3085:GvCwJOd45rjkWANH@cluster0.hjin0iy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 client = MongoClient(mongo_uri, tls=True, tlsAllowInvalidCertificates=True)
 db = client['know-your-sleep-genetics']  # Use your database name
 users_collection = db['users']
